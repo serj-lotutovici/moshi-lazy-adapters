5f87027339c11f0429e9f9c124629414801a369f
Anton Dauvalter <adauvalter@gmail.com>
2017-02-16 11:03:04 +0100

    Introduce @LastElement annotation plus adapter (#50)

---
+++ src/main/java/com/serjltt/moshi/adapters/LastElement.java
@@ -0,0 +1,65 @@
+package com.serjltt.moshi.adapters;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.JsonQualifier;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Type;
+import java.util.Set;
+
+/**
+ * Indicates that the annotated type/field is <strong>expected</strong> to be the last element of a
+ * json array.
+ *
+ * <p>For example if a json object is returned as:
+ * <pre>
+ *   [
+ *    {
+ *      "some_field": "some_value",
+ *      "other_field": "other_value"
+ *    }
+ *   ]
+ * </pre>
+ * And the consumer only cares about the last element, in the case of using a retrofit service
+ * method the code would look like:
+ *
+ * <pre><code>
+ *   {@literal @}GET("path/")
+ *   {@literal @}LastElement Call&lt;DataObject&gt; getData();
+ * </code></pre>
+ *
+ * The resulting response returned by {@code response.body()} will be an instance of {@code
+ * DataObject} with the respective values set.
+ *
+ * <p>To leverage from {@link LastElement} {@linkplain LastElement#ADAPTER_FACTORY}
+ * must be added to a {@linkplain Moshi Moshi instance}:
+ *
+ * <pre><code>
+ *   Moshi moshi = new Moshi.Builder()
+ *      .add(LastElement.ADAPTER_FACTORY)
+ *      .build();
+ * </code></pre>
+ */
+@Documented
+@JsonQualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.METHOD })
+public @interface LastElement {
+  /** Builds an adapter that can process a type/field annotated with {@link LastElement}. */
+  JsonAdapter.Factory ADAPTER_FACTORY = new JsonAdapter.Factory() {
+    @Override public JsonAdapter<?> create(Type type, Set<? extends Annotation> annotations,
+        Moshi moshi) {
+      Set<? extends Annotation> nextAnnotations =
+          Types.nextAnnotations(annotations, LastElement.class);
+      if (nextAnnotations == null || !nextAnnotations.isEmpty()) return null;
+
+      return new LastElementJsonAdapter<>(type, moshi);
+    }
+  };
+}
---
+++ src/unitTest/java/com/serjltt/moshi/adapters/LastElementJsonAdapterTest.java
@@ -0,0 +1,143 @@
+package com.serjltt.moshi.adapters;
+
+import com.squareup.moshi.Json;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.JsonDataException;
+import com.squareup.moshi.Moshi;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+public final class LastElementJsonAdapterTest {
+  // Lazy adapters work only within the context of moshi.
+  private final Moshi moshi = new Moshi.Builder()
+      .add(LastElement.ADAPTER_FACTORY)
+      .add(new Custom.CustomAdapter()) // We need to check that other annotations are not lost.
+      .build();
+
+  @Test public void last() throws Exception {
+    JsonAdapter<LastElementJsonAdapterTest.Data>
+        adapter = moshi.adapter(LastElementJsonAdapterTest.Data.class);
+
+    LastElementJsonAdapterTest.Data fromJson = adapter.fromJson("{\n"
+        + "  \"obj\": [\n"
+        + "    \"one\",\n"
+        + "    \"two\"\n"
+        + "  ]\n"
+        + "}");
+    assertThat(fromJson.str).isEqualTo("two");
+
+    String toJson = adapter.toJson(fromJson);
+    // The excluded data is lost during parsing
+    // Adapter under test assumes that the consumer doesn't need that data
+    assertThat(toJson).isEqualTo("{\"obj\":[\"two\"]}");
+  }
+
+  @Test public void fromJsonOnEmptyArrayReturnsNull() throws Exception {
+    assertNullReturn("{\n"
+        + "  \"obj\": []\n"
+        + "}");
+  }
+
+  @Test public void fromJsonOnNullArrayReturnsNull() throws Exception {
+    assertNullReturn("{\n"
+        + "  \"obj\": null\n"
+        + "}");
+  }
+
+  @Test public void fromJsonExpectsAnArray() throws Exception {
+    JsonAdapter<LastElementJsonAdapterTest.Data> adapter = moshi.adapter(Data.class);
+
+    try {
+      adapter.fromJson("{\n"
+          + "  \"obj\": \"this_will_throw\"\n"
+          + "}");
+      fail();
+    } catch (JsonDataException e) {
+      // Moshi's Collection adapter will throw
+      assertThat(e).hasMessage("Expected BEGIN_ARRAY but was STRING at path $.obj");
+    }
+  }
+
+  // Currently there is no way to create an adapter
+  @Test @Ignore public void factoryMaintainsOtherAnnotations() throws Exception {
+    JsonAdapter<LastElementJsonAdapterTest.Data2> adapter = moshi.adapter(Data2.class);
+
+    LastElementJsonAdapterTest.Data2 fromJson = adapter.fromJson("{\n"
+        + "  \"str\": [\n"
+        + "    \"test\"\n"
+        + "  ]\n"
+        + "}");
+    assertThat(fromJson.str).isEqualTo("testCustom");
+
+    String toJson = adapter.toJson(fromJson);
+    assertThat(toJson).isEqualTo("{\"str\":[\"test\"]}");
+  }
+
+  // This one is redundant, but keeps JaCoCo quiet
+  @Test public void factoryExpectsOnlyOneAnnotation() throws Exception {
+    // A list of fake annotations.
+    Set<Annotation> annotations = new LinkedHashSet<Annotation>() {
+      {
+        add(new Annotation() {
+          @Override public Class<? extends Annotation> annotationType() {
+            return Test.class;
+          }
+        });
+        add(new Annotation() {
+          @Override public Class<? extends Annotation> annotationType() {
+            return Custom.class;
+          }
+        });
+      }
+    };
+    assertThat(LastElement.ADAPTER_FACTORY.create(String.class, annotations, moshi)).isNull();
+
+    // Emulate existing annotation (should also return null).
+    annotations.add(new Annotation() {
+      @Override public Class<? extends Annotation> annotationType() {
+        return LastElement.class;
+      }
+    });
+    assertThat(LastElement.ADAPTER_FACTORY.create(String.class, annotations, moshi)).isNull();
+  }
+
+  @Test public void toStringReflectsInnerAdapter() throws Exception {
+    JsonAdapter<String> adapter = moshi.adapter(String.class,
+        Collections.singleton(new LastElement() {
+          @Override public Class<? extends Annotation> annotationType() {
+            return LastElement.class;
+          }
+        }));
+
+    assertThat(adapter.toString())
+        .isEqualTo("JsonAdapter(String).nullSafe().collection().nullSafe().lastElement()");
+  }
+
+  private void assertNullReturn(String string) throws IOException {
+    JsonAdapter<LastElementJsonAdapterTest.Data> adapter = moshi.adapter(Data.class);
+
+    LastElementJsonAdapterTest.Data fromJson = adapter.fromJson(string);
+    assertThat(fromJson.str).isNull();
+
+    String toJson = adapter.toJson(fromJson);
+    assertThat(toJson).isEqualTo("{\"obj\":[null]}");
+  }
+
+  private static class Data {
+    @LastElement
+    @Json(name = "obj") String str;
+  }
+
+  private static class Data2 {
+    @LastElement
+    @Custom String str;
+  }
+}
